36d89e1df7f79429c6181a28c5da870c2debd796
David Flores <slither366@gmail.com>
2019-03-10 02:26:51 -0500

    Dflores: 10/03/19 se mejoro el diseño del Frontend del App y se cambio el nombre PdApp version 1.0

--- app/src/main/java/com/example/farmaapp/MainActivity.java
+++ app/src/main/java/com/example/farmaapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.farmaapp.fragments.ConteoFragment;
 import com.example.farmaapp.fragments.InventarioFragment;
@@ -19,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
         tabLayoutPrincipal = findViewById(R.id.TabLayoutPrincipal);
         vpContenido = findViewById(R.id.vpContenido);
