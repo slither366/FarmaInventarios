7ebb84b6635a374b45212e5c4b5ee1787f7773b8
David Flores <slither366@gmail.com>
2019-03-11 07:37:41 -0500

    Dflores: 11/03/19 implementando eventos

--- app/src/main/java/com/example/farmaapp/MainActivity.java
+++ app/src/main/java/com/example/farmaapp/MainActivity.java
@@ -1,22 +1,31 @@
 package com.example.farmaapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.farmaapp.fragments.ConteoFragment;
 import com.example.farmaapp.fragments.InventarioFragment;
 import com.example.farmaapp.fragments.VpAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TabLayout tabLayoutPrincipal;
     ViewPager vpContenido;
+    Integer vCantEntero, vCantFraccion;
+
+    FloatingActionButton fab_entero_menos, fab_entero_mas, fab_fraccion_menos, fab_fraccion_mas;
+    EditText tv_entero, tv_fraccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,35 @@ public class MainActivity extends AppCompatActivity {
         //tabLayoutPrincipal.addTab(tabLayoutPrincipal.newTab().setText("Inventario"));
 
         configurarContenido();
+
+        //inicializarUI();
+
+        //iniciarEventos();
+
+    }
+
+    private void inicializarUI() {
+
+        fab_entero_mas = (FloatingActionButton)findViewById(R.id.fab_entero_mas);
+        fab_entero_menos = (FloatingActionButton)findViewById(R.id.fab_entero_menos);
+        fab_fraccion_mas = (FloatingActionButton)findViewById(R.id.fab_fraccion_mas);
+        fab_fraccion_menos = (FloatingActionButton)findViewById(R.id.fab_fraccion_menos);
+
+        tv_entero = findViewById(R.id.tv_entero);
+        tv_fraccion = findViewById(R.id.tv_fraccion);
+
+        this.vCantEntero = 0;
+        this.vCantFraccion = 0;
+
+    }
+
+    private void iniciarEventos() {
+
+        fab_entero_mas.setOnClickListener(this);
+        //btn_entero_menos.setOnClickListener(this);
+        //btn_fraccion_mas.setOnClickListener(this);
+        //btn_fraccion_menos.setOnClickListener(this);
+
     }
 
     private void configurarContenido(){
@@ -56,5 +94,61 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    private void disminuirUnaFraccion() {
+        this.vCantFraccion = this.vCantFraccion - 1;
+        tv_fraccion.setText(this.vCantFraccion+" ");
+    }
+
+    private void agregarUnaFraccion() {
+        this.vCantFraccion = this.vCantFraccion + 1;
+        tv_fraccion.setText(this.vCantFraccion+" ");
+    }
+
+    private void disminuirUnEntero() {
+
+        this.vCantEntero = this.vCantEntero - 1;
+        tv_entero.setText(this.vCantEntero+" ");
+
+    }
+
+    private void agregarUnEntero() {
+
+        this.vCantEntero = this.vCantEntero + 1;
+        tv_entero.setText(this.vCantEntero+" ");
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.fab_entero_mas:
+                Toast.makeText(this, "Click en Entero Mas!", Toast.LENGTH_SHORT).show();
+                //agregarUnEntero();
+                break;
+
+            case R.id.fab_entero_menos:
+                Toast.makeText(this, "Click en Entero Menos!", Toast.LENGTH_SHORT).show();
+                //disminuirUnEntero();
+                break;
+
+            case R.id.fab_fraccion_mas:
+                Toast.makeText(this, "Click en Fraccion Mas", Toast.LENGTH_SHORT).show();
+                //agregarUnaFraccion();
+                break;
+
+            case R.id.fab_fraccion_menos:
+                Toast.makeText(this, "Click en Fraccion Menos", Toast.LENGTH_SHORT).show();
+                //disminuirUnaFraccion();
+                break;
+
+        }
+
+        tv_entero.setText(this.vCantEntero);
+        tv_fraccion.setText(this.vCantFraccion);
+
     }
 }
--- app/src/main/java/com/example/farmaapp/fragments/ConteoFragment.java
+++ app/src/main/java/com/example/farmaapp/fragments/ConteoFragment.java
@@ -2,18 +2,27 @@ package com.example.farmaapp.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.farmaapp.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ConteoFragment extends Fragment {
+public class ConteoFragment extends Fragment implements View.OnClickListener {
 
+    View v;
+    Integer vCantEntero, vCantFraccion;
+
+    FloatingActionButton fab_entero_menos, fab_entero_mas, fab_fraccion_menos, fab_fraccion_mas;
+    EditText tv_entero, tv_fraccion;
 
     public ConteoFragment() {
         // Required empty public constructor
@@ -24,7 +33,58 @@ public class ConteoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contador, container, false);
+
+        v = inflater.inflate(R.layout.fragment_contador, container, false);
+
+        inicializarUI();
+
+        iniciarEventos();
+
+        return v;
+
     }
 
+    private void iniciarEventos() {
+
+        fab_entero_mas.setOnClickListener(this);
+
+    }
+
+    private void inicializarUI() {
+
+        fab_entero_mas = (FloatingActionButton) v.findViewById(R.id.fab_entero_mas);
+        fab_entero_menos = (FloatingActionButton) v.findViewById(R.id.fab_entero_menos);
+        fab_fraccion_mas = (FloatingActionButton) v.findViewById(R.id.fab_fraccion_mas);
+        fab_fraccion_menos = (FloatingActionButton) v.findViewById(R.id.fab_fraccion_menos);
+
+        tv_entero = v.findViewById(R.id.tv_entero);
+        tv_fraccion = v.findViewById(R.id.tv_fraccion);
+
+        this.vCantEntero = 0;
+        this.vCantFraccion = 0;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.fab_entero_mas:
+                agregarUnEntero();
+                break;
+
+        }
+
+        this.tv_entero.setText(this.vCantEntero);
+        this.tv_fraccion.setText(this.vCantFraccion);
+
+    }
+
+    private void agregarUnEntero() {
+        this.tv_entero.setText("1");
+        //this.vCantEntero = this.vCantEntero + 1;
+        //this.tv_entero.setText(this.vCantEntero + " ");
+
+    }
 }
